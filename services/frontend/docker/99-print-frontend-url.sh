5616ef193341d2efbd82b72ae71d44274d11b114
ilyaytrewq <tixonov.ilay.2000@mail.ru>
2026-05-09 03:29:15 +0300

    dev

--- services/frontend/docker/99-print-frontend-url.sh
+++ services/frontend/docker/99-print-frontend-url.sh
@@ -3,4 +3,7 @@ set -eu
 
 public_url="${FRONTEND_PUBLIC_URL:-http://localhost:5173}"
 
-printf '\nFrontend is available at: %s\n\n' "$public_url"
+printf '\nFrontend is available at: %s\n' "$public_url"
+printf '\nVK (Vite, same values as compose / runtime env used at image build):\n'
+printf '  VITE_VK_APP_ID=%s\n' "${VITE_VK_APP_ID:-<not set>}"
+printf '  VITE_VK_REDIRECT_URI=%s\n\n' "${VITE_VK_REDIRECT_URI:-<not set>}"
